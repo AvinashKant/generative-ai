4a334dbb4781c1bbda014a70b29f83d30a8792b8
Avinash kant <contact.avinashkantse@gmail.com>
2025-08-11 22:25:17 +0530

    LLM Work

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # generative-ai
+
+# create virtual env
+- python3 -m venv genai
+
+# Activate the virtual environment:
+- .\genai\Scripts\activate
+
+# Deactivate to exit the virtual environment:
+- deactivate
+
+- python index.py
+
+# set variable
+genai\Scripts\activate
+set API_KEY=my-secret-key
+python script.py
--- index.py
+++ index.py
@@ -1,1 +1,16 @@
+from google import genai
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Reads .env and loads variables into environment
+api_key = os.getenv("GEMINI_API_KEY")
+print(api_key)
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
